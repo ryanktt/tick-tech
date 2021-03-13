c8d5176eaaa2f8483880934fdd7b16c166aa4843
ryanktt <ryankayrorv@outlook.com>
2021-03-13 16:45:43 -0300

    final touches

--- controllers/client.js
+++ controllers/client.js
@@ -1,4 +1,6 @@
 const Post = require('../models/post.js');
+const User = require('../models/user.js');
+const moment = require('moment')
 
 exports.getShowPost = (req, res, next) => {
     const postId = req.params.id;
@@ -9,12 +11,20 @@ exports.getShowPost = (req, res, next) => {
     })
     .then(post => {
         const tagArr = post.tags.split(',');
+        let date = new Date(post.createdAt);
+        let createdAt = moment(date).format('l');
         
-        return res.render('../views/post.ejs', {
-            post: post, 
-            tagArr: tagArr,
-            pageTitle: post.title
-        });
+        User.findById(post.user_id)
+        .then(user => {
+            user = user[0][0];
+            return res.render('../views/post.ejs', {
+                post: post, 
+                tagArr: tagArr,
+                pageTitle: post.title,
+                user: user,
+                createdAt: createdAt
+            });
+        })
     })
     .catch(err => {
       next();
@@ -49,12 +59,22 @@ exports.getShowPosts = (req, res, next) => {
 }
 
 exports.getShowPostsSearch = (req, res, next)=> {
+    function truncateString(str, num) {
+        if (num > str.length){
+          return str;
+        } else{
+          str = str.substring(0,num);
+          return str+"...";
+        }
+      
+      }
     const subject = req.body.subject;
     Post.search(subject)
     .then(posts => {
         res.render('../views/index.ejs', {
             posts: posts, 
-            pageTitle: 'Search: ' + subject + ''
+            pageTitle: 'Search: ' + subject + '',
+            truncateString: truncateString
         });
     })
     .catch(err => {
